249b2d27bc64ea230e1888a7904c7292925ff0ae
Frank Anneveld <anneveld@Franks-MacBook-Pro.local>
2020-09-24 08:21:44 +0200

    layee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public icon: string;
+  public xpos: number = 0;
+  public ypos: number = 600;
+
+
   public icons = [
       {
         name: 'Circle',
@@ -25,4 +30,15 @@ export class AppComponent {
         path: 'https://upload.wikimedia.org/wikipedia/commons/5/5d/Duke_%28Java_mascot%29_waving.svg'
       }
     ]
+
+    constructor() {
+      interval(10).subscribe( t => {
+        this.xpos += 2;
+        if (this.xpos > 900) this.xpos = 0;
+        // this.xpos = Math.floor(Math.random() * 800);
+        this.ypos = 300;
+        // this.ypos = Math.floor(Math.random() * 600);
+      });
+
+    }
 }
